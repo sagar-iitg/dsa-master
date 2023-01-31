a7df6fcd678280f76011ff914a94931792dd8047
sagar <sagar-iitg>
2023-01-31 11:58:48 +0530

    Hollow rectangle code

---
+++ VSCode/JAVA/ForLoop.java
@@ -0,0 +1,21 @@
+public class ForLoop {
+
+    public static void main(String[] args) {
+        
+        int i=0;
+        for(;;)
+        {
+            if(i<5)
+            {
+                System.out.println(i);
+                i++;
+              
+            }
+            System.out.println("hello world");
+          
+           
+
+        }
+    }
+    
+}
--- VSCode/JAVA/HelloWorld.java
+++ VSCode/JAVA/HelloWorld.java
@@ -6,6 +6,12 @@ class HelloWorld{
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        for(int i=0;i<5;i++)
+        {
+            System.out.println(i);
+           
+        }
     }
 
 
