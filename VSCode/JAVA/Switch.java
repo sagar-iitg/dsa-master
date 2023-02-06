12c35bbdb7ad605262c6ca744a8047a1096d849c
sagar <sagar-iitg>
2023-02-06 19:29:40 +0530

    switch string

--- VSCode/JAVA/Switch.java
+++ VSCode/JAVA/Switch.java
@@ -1,9 +1,13 @@
 public class Switch {
 
 
+
+    //Readibility wise switch is better then if else
+
     public static void main(String[] args) {
 
         int ch=2;
+        
         switch(ch)
         {
                 case 1: System.out.println("Case1");
