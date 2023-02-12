6e94a656c00661902e5bd436823fa53b88a7d809
sagar <sagar-iitg>
2023-02-12 22:18:47 +0530

    added swap method in java

---
+++ VSCode/JAVA/swap/Method1.java
@@ -0,0 +1,19 @@
+package swap;
+
+public class Method1 {
+
+
+    public static void main(String[] args) {
+        
+
+
+        int x=10;
+        int y=5;
+        int temp=x;
+        x=y;
+        y=temp;
+        System.out.println(x+" "+y);
+
+    }
+    
+}
