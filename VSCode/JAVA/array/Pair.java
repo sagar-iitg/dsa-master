13b497797bb8e65803179b6c1da3f4425effc8e0
sagar <sagar-iitg>
2023-02-12 13:41:17 +0530

    added array2d pkg

---
+++ VSCode/JAVA/array/Pair.java
@@ -0,0 +1,23 @@
+package array;
+import java.util.ArrayList;
+
+public class Pair {
+
+
+    public static void main(String[] args) {
+        
+        ArrayList<Integer> arr=new ArrayList<>();
+        arr.add(0, 1);
+        arr.add(25);
+
+        arr.forEach(System.out::println);
+
+        
+
+
+
+    }
+
+
+    
+}
