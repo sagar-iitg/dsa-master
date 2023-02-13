f33c1592ec0b93054847947b71b83c2f48a16ba7
sagar <sagar-iitg>
2023-02-13 17:46:04 +0530

    Driver and Hash Set Method

--- VSCode/JAVA/array/union/Driver.java
+++ VSCode/JAVA/array/union/Driver.java
@@ -7,10 +7,15 @@ public class Driver {
 
     public static void main(String[] args) {
         
-        int[] arr1={2,6,8};
-        int[] arr2={1,3,5,6,7,9,10};
+        // int[] arr1={2,6,8};
+        // int[] arr2={1,3,5,6,7,9,10};
+
+        
+        int arr1[] = { 1, 2, 5, 6, 2, 3, 5, 7, 3 };
+        int arr2[] = { 2, 4, 5, 6, 8, 9, 4, 6, 5, 4 };
+
         ArrayList<Integer> res=new ArrayList<>();
-        Union u=new UnionOf2Array();
+        Union u=new HashSetMethod();
         res=u.union(arr1, arr2);
         System.out.println(res);
         
--- VSCode/JAVA/array/union/HashSetMethod.java
+++ VSCode/JAVA/array/union/HashSetMethod.java
@@ -1,6 +1,8 @@
 package array.union;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class HashSetMethod implements Union {
 
@@ -12,8 +14,25 @@ public class HashSetMethod implements Union {
     public ArrayList<Integer> union(int[] arr1,int[] arr2) {
         
 
+       
+
+
+        Set<Integer> s=new HashSet<>();
+
+        for (Integer integer : arr1) {
+           s.add(integer);
+            
+        }
+
+        for (Integer integer : arr2) {
+            s.add(integer);
+             
+         }
+         ArrayList<Integer> res=new ArrayList<>(s);
+        return res;
+
         
-        return null;
+        
     }
     
 }
