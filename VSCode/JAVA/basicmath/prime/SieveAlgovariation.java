08f57d5d0cccb1c1c6de83ffbfe5bc202a3a122a
Sagar Kumar <sagar-iitg>
2023-03-07 21:01:28 +0530

    Sieve algo

--- VSCode/JAVA/basicmath/prime/SieveAlgovariation.java
+++ VSCode/JAVA/basicmath/prime/SieveAlgovariation.java
@@ -1,33 +1,39 @@
 package basicmath.prime;
 
-public class SieveAlgo {
+public class SieveAlgovariation {
     
     public static void main(String[] args) {
         
-        boolean ans=isPrime(-5);
+        primeNo(1,50);
         //false means prime
         //true means not prime;
-        System.out.println(!ans);
+        //System.out.println(!ans);
 
 
     }
+    private static void primeNo(int l,int h)
+    {
+        for(int i=l;i<=h;i++)
+        {
+            if(isPrime(i)){
+                System.out.print(i+" ");
+            }
+        }
+    }
+
+
+
 
     private static boolean isPrime(int n)
     {
 
 
-        if(n<=1 && n>=0)
-        {
-            return true;
-        }
-        if(n<0)
-        {
-            System.out.println("Invalid Input");
-            System.exit(0);
-        }
+        if(n<=1)
+            return false;
+
         boolean[] primes=new boolean[n+1];
-        //false means prime
-        //true means not prime;
+
+      
 
         for(int i=2;i*i<=n;i++)
         {
@@ -40,7 +46,17 @@ public class SieveAlgo {
                 }
             }
         }
-        return primes[n];
+
+        if(!primes[n])
+        {
+            return true;
+        }
+
+        return false;
+
+
+
+
 
 
         
