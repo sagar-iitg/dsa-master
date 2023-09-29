710346b885e8617732945b0d0c53492082eb6b46
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2023-09-29 16:39:18 +0530

    added checkIsEmpty String

---
+++ src/main/java/string/CheckIsEmpty.java
@@ -0,0 +1,11 @@
+package string;
+
+public class CheckIsEmpty {
+
+    public static void main(String[] args) {
+        StringBuilder s=new StringBuilder("");
+        System.out.println(s.isEmpty());
+        String a=" ";
+        System.out.println(a.isEmpty());
+    }
+}
